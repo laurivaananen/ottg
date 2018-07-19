1b7590a2c37bba087b8e7d603da914aa65594fbf
Lauri Väänänen <lauri.e.helsinki.fi>
2018-07-19 16:48:04 +0300

    Custom login system

--- superlists/urls.py
+++ superlists/urls.py
@@ -17,8 +17,10 @@ from django.conf.urls import include
 from django.urls import path
 from lists import views as list_views
 from lists import urls as list_urls
+from accounts import urls as accounts_urls
 
 urlpatterns = [
     path('', list_views.home_page, name='home'),
     path('lists/', include(list_urls)),
+    path('accounts/', include(accounts_urls)),
 ]
