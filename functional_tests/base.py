de3564c86532ffaa2633349587ddd9a1f79d2029
Lauri Väänänen <lauri.e.helsinki.fi>
2018-08-06 15:51:45 +0300

    Added wait decorator

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -6,6 +6,18 @@ import os
 
 MAX_WAIT = 10
 
+def wait(fn):
+    def modified_fn(*args, **kwargs):
+        start_time = time.time()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+    return modified_fn
+
 class FunctionalTest(StaticLiveServerTestCase):
 
     def setUp(self):
@@ -30,25 +42,16 @@ class FunctionalTest(StaticLiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
+    @wait
     def wait_for(self, fn):
-        start_time = time.time()
-        while True:
-            try:
-                return fn()
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
+        return fn()
 
     def get_item_input_box(self):
         return self.browser.find_element_by_id('id_text')
 
+    @wait
     def wait_to_be_logged_in(self, email):
-        self.wait_for(
-            lambda: self.browser.find_element_by_link_text('Log out')
-        )
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn(email, navbar.text)
+        lambda: self.browser.find_element_by_link_text('Log out')
 
     def wait_to_be_logged_out(self, email):
         self.wait_for(
@@ -56,3 +59,5 @@ class FunctionalTest(StaticLiveServerTestCase):
         )
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertNotIn(email, navbar.text)
+
+
